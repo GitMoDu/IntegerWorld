71455a0dc5f0fd50b9f04a0592d73419506a9720
GitMoDu <batatas@gmail.com>
2025-06-21 21:09:30 +0100

    Added engine tasks.

---
+++ src/IntegerWorldOutputs.h
@@ -0,0 +1,20 @@
+#ifndef _INTEGER_WORLD_OUTPUTS_INCLUDE_h
+#define _INTEGER_WORLD_OUTPUTS_INCLUDE_h
+
+#include "OutputSurfaces/MockSurface.h"
+
+#if defined(ARDUINO)
+#include "OutputSurfaces/Adafruit/AdafruitSurfaceSSD1306.h"
+#include "OutputSurfaces/Adafruit/AdafruitSurfaceSH1106.h"
+#include "OutputSurfaces/Adafruit/AdafruitSurfaceSSD1331.h"
+#include "OutputSurfaces/Adafruit/AdafruitSurfaceST7735.h"
+#include "OutputSurfaces/Adafruit/AdafruitSurfaceST7789.h"
+
+#if defined(ARDUINO_ARCH_AVR)
+#include "OutputSurfaces/Lexus2kSurfaceSsd1306.h"
+#endif
+#endif
+
+#include "OutputSurfaces/EgfxSurface.h"
+
+#endif
---
+++ src/IntegerWorldTasks.h
@@ -0,0 +1,7 @@
+#ifndef _INTEGER_WORLD_TASKS_INCLUDE_h
+#define _INTEGER_WORLD_TASKS_INCLUDE_h
+
+#include "Engine/EngineRenderTask.h"
+#include "Engine/PerformanceLogTask.h"
+
+#endif
