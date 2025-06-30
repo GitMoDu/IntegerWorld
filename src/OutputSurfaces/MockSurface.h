5b1ecede76e2c13f140b3cab43b43797815bff7f
GitMoDu <batatas@gmail.com>
2025-06-30 13:32:30 +0100

    Updated mock surface to latest spec.

--- src/OutputSurfaces/MockSurface.h
+++ src/OutputSurfaces/MockSurface.h
@@ -10,11 +10,13 @@ namespace IntegerWorld
 		template<uint16_t surfaceWidth, uint16_t surfaceHeight>
 		struct OutputSurface : IOutputSurface
 		{
+			virtual bool StartSurface() { return true; }
+
 			virtual bool IsSurfaceReady() { return true; }
 
 			virtual void GetSurfaceDimensions(int16_t& width, int16_t& height, uint8_t& colorDepth)
 			{
-				width = surfaceWidth; height = surfaceHeight; colorDepth = 1;
+				width = surfaceWidth; height = surfaceHeight; colorDepth = 16;
 			}
 
 #if defined(ARDUINO)
