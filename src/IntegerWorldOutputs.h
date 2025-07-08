8254a91d6f0faff5555645acf31c4183a466217c
GitMoDu <batatas@gmail.com>
2025-07-08 19:33:07 +0100

    Excluded incompatible drivers.

--- src/IntegerWorldOutputs.h
+++ src/IntegerWorldOutputs.h
@@ -7,8 +7,10 @@
 #include "OutputSurfaces/Adafruit/AdafruitSurfaceSSD1306.h"
 #include "OutputSurfaces/Adafruit/AdafruitSurfaceSH1106.h"
 #include "OutputSurfaces/Adafruit/AdafruitSurfaceSSD1331.h"
+#if !defined(ARDUINO_ARCH_STM32F4)
 #include "OutputSurfaces/Adafruit/AdafruitSurfaceST7735.h"
 #include "OutputSurfaces/Adafruit/AdafruitSurfaceST7789.h"
+#endif
 
 #if defined(ARDUINO_ARCH_AVR)
 #include "OutputSurfaces/Lexus2kSurfaceSsd1306.h"
