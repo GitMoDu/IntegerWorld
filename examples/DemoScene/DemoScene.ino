fad16479c9f37e1bdde98a89e9179b7c8f65aadd
GitMoDu <batatas@gmail.com>
2025-07-19 15:02:52 +0100

    Updated examples for new includes.

--- examples/DemoScene/DemoScene.ino
+++ examples/DemoScene/DemoScene.ino
@@ -12,12 +12,25 @@
 #define _TASK_OO_CALLBACKS
 #include <TScheduler.hpp>
 
+#include <SPI.h>
+#include <Wire.h>
+
 #include <IntegerWorld.h>
 #include <IntegerWorldTasks.h>
 #include <IntegerWorldOutputs.h>
+
+// Include used output drivers. 
+// Each driver has its own dependencies, so you may need to install them.
+// Some drivers may not be included at the same time, as they may conflict, such as SumotoySsd1331 and Adafruit's.
+//#include <IntegerWorldAdafruitSsd1306.h>
+//#include <IntegerWorldAdafruitSh1106.h>
+//#include <IntegerWorldAdafruitSsd1331.h>
+//#include <IntegerWorldAdafruitSt77XX.h>
+//#include <IntegerWorldLexus2kSsd1306.h>
+//#include <IntegerWorldSumotoySsd1331.h>
+
 #include "AnimatedDemoScene.h"
 #include "DemoSceneAssets.h"
-
 
 // Forward declare the used communications hardware.
 #if defined(ARDUINO_MAPLE_MINI)
@@ -37,24 +50,29 @@ SPIClass& SpiInstance(SPI);
 #define TFT_CS		D3
 #define TFT_DC		D7
 #define TFT_RST     D6
-#define TFT_BKL     -1//D2
+#elif defined(ARDUINO_ARCH_ESP32)
+#define TFT_CS		5
+#define TFT_DC		16
+#define TFT_RST     23
 #else
 #define TFT_CS		10
 #define TFT_DC		9
 #define TFT_RST     8
-#define TFT_BKL     -1
 #endif
 
 
 
-// Uncomment output target driver to use.
-//IntegerWorld::Lexus2k::Ssd1306I2c::DirectDrawSurface Driver{}; // https://github.com/lexus2k/ssd1306
-//IntegerWorld::Adafruit::St7789::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
-//IntegerWorld::Adafruit::St7735::DirectDrawSurface Driver(IntegerWorld::Adafruit::St7735::VariantEnum::Mini160x80Plugin, SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
-//IntegerWorld::Adafruit::Ssd1331::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino
+// Uncomment output driver to use.
 //IntegerWorld::Adafruit::Ssd1306I2c::FramebufferSurface Driver(Wire); // https://github.com/adafruit/Adafruit_SSD1306
 //IntegerWorld::Adafruit::Ssd1306Spi::FramebufferSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit_SSD1306
-//IntegerWorld::Adafruit::Sh1106Spi::FramebufferSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST, 0); // https://github.com/adafruit/Adafruit_SH110X
+//IntegerWorld::Adafruit::Sh1106Spi::FramebufferSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit_SH110X
+//IntegerWorld::Adafruit::Ssd1331::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino
+//IntegerWorld::Adafruit::St7735::DirectDrawSurface Driver(IntegerWorld::Adafruit::St7735::VariantEnum::Mini160x80Plugin, SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
+//IntegerWorld::Adafruit::St7789::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit-ST7735-Library
+
+//IntegerWorld::Lexus2k::Ssd1306I2c::DirectDrawSurface Driver{}; // https://github.com/lexus2k/ssd1306
+
+//IntegerWorld::Sumotoy::Ssd1331::DirectDrawSurface Driver(TFT_CS, TFT_DC, TFT_RST); // https://github.com/sumotoy/SSD_13XX
 
 //IntegerWorld::MockOutput::OutputSurface<128, 128> Driver{}; // Mock output.
 
--- examples/TinyScene/TinyScene.ino
+++ examples/TinyScene/TinyScene.ino
@@ -1,26 +1,40 @@
 /*
 * Direct Draw Tiny Scene.
-* Includes configurations for multiple screens (uncomment screen driver).
-* Drivers draw directly to screen (no framebuffer).
 * Tiny scene with no light or scene shader that fits in an AVR328 (Arduino OG).
+* Includes configurations for multiple screen drivers (uncomment driver and required include).
 * With DEMO_SCENE_EDGE_OBJECT: 2 edge objects. Without: 2 mesh objects.
 */
 
 #define DEBUG
 #define SERIAL_BAUD_RATE 115200
 
-#define DEMO_SCENE_EDGE_OBJECT // Enable for monochrome and low RAM devices.
+
+//#define DEMO_SCENE_EDGE_OBJECT // Enable for monochrome and low RAM devices.
 #define INTEGER_WORLD_PERFORMANCE_LOG // Enable engine render status logging.
 #define INTEGER_WORLD_PERFORMANCE_DEBUG // Enable engine debug level status measuring.
+
 
 #define _TASK_OO_CALLBACKS
 #include <TScheduler.hpp>
 
+#include <SPI.h>
+#include <Wire.h>
+
 #include <IntegerWorld.h>
 #include <IntegerWorldTasks.h>
 #include <IntegerWorldOutputs.h>
-#include "AnimatedTinyScene.h"
 
+// Include used output drivers. 
+// Each driver has its own dependencies, so you may need to install them.
+// Some drivers may not be included at the same time, as they may conflict, such as SumotoySsd1331 and Adafruit's.
+//#include <IntegerWorldAdafruitSsd1306.h>
+//#include <IntegerWorldAdafruitSh1106.h>
+//#include <IntegerWorldAdafruitSsd1331.h>
+//#include <IntegerWorldAdafruitSt77XX.h>
+//#include <IntegerWorldLexus2kSsd1306.h>
+//#include <IntegerWorldSumotoySsd1331.h>
+
+#include "AnimatedTinyScene.h"
 
 // Forward declare the used communications hardware.
 #if defined(ARDUINO_MAPLE_MINI)
@@ -32,14 +46,17 @@
 #define TFT_CS		D3
 #define TFT_DC		D7
 #define TFT_RST     D6
-#define TFT_BKL     -1
+#elif defined(ARDUINO_ARCH_ESP32)
+#define TFT_CS		5
+#define TFT_DC		16
+#define TFT_RST     23
 #else
 #define TFT_CS		10
 #define TFT_DC		9
 #define TFT_RST     8
-#define TFT_BKL     -1
 #endif
-#if defined(ARDUINO_MAPLE_MINI)
+
+#if defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_STM32F4)
 SPIClass SpiInstance(1);
 #elif defined(ARDUINO_ARCH_RP2040)
 SPIClassRP2040& SpiInstance(SPI);
@@ -48,14 +65,17 @@ SPIClass& SpiInstance(SPI);
 #endif
 
 
-// Uncomment output target to use.
-//IntegerWorld::Lexus2k::Ssd1306I2c::DirectDrawSurface Driver{}; // https://github.com/lexus2k/ssd1306
-//IntegerWorld::Adafruit::St7789::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
-//IntegerWorld::Adafruit::St7735::DirectDrawSurface Driver(IntegerWorld::Adafruit::St7735::VariantEnum::Mini160x80Plugin, SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
-//IntegerWorld::Adafruit::Ssd1331::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST, false, 0); // https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino
+// Uncomment output driver to use.
 //IntegerWorld::Adafruit::Ssd1306I2c::FramebufferSurface Driver(Wire); // https://github.com/adafruit/Adafruit_SSD1306
 //IntegerWorld::Adafruit::Ssd1306Spi::FramebufferSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit_SSD1306
 //IntegerWorld::Adafruit::Sh1106Spi::FramebufferSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit_SH110X
+//IntegerWorld::Adafruit::Ssd1331::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino
+//IntegerWorld::Adafruit::St7735::DirectDrawSurface Driver(IntegerWorld::Adafruit::St7735::VariantEnum::Mini160x80Plugin, SpiInstance, TFT_CS, TFT_DC, TFT_RST, TFT_BKL); // https://github.com/adafruit/Adafruit-ST7735-Library
+//IntegerWorld::Adafruit::St7789::DirectDrawSurface Driver(SpiInstance, TFT_CS, TFT_DC, TFT_RST); // https://github.com/adafruit/Adafruit-ST7735-Library
+
+//IntegerWorld::Lexus2k::Ssd1306I2c::DirectDrawSurface Driver{}; // https://github.com/lexus2k/ssd1306
+
+//IntegerWorld::Sumotoy::Ssd1331::DirectDrawSurface Driver(TFT_CS, TFT_DC, TFT_RST); // https://github.com/sumotoy/SSD_13XX
 
 //IntegerWorld::MockOutput::OutputSurface<128, 128> Driver{}; // Mock output.
 
