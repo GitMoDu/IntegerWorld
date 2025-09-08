7c300676d2b466a2ee43f348173ff2b2c5d4393b
André <batatas@gmail.com>
2025-09-08 23:46:52 +0100

    Revert "Migrate IntegerWorld from legacy IntegerSignal Fraction and Resize namespaces to FixedPoint Q-format APIs"

--- src/Object/PointObject.h
+++ src/Object/PointObject.h
@@ -33,7 +33,7 @@ namespace IntegerWorld
 	{
 	public:
 		rotation_angle_t Rotation{};
-		Scale16::factor_t Resize = Scale16::SCALE_1X;
+		resize16_t Resize = RESIZE16_1X;
 
 	protected:
 		transform16_scale_rotate_translate_t MeshTransform{};
